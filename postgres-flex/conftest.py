158d488a6ddba001ac81632ffc21720c43fc2690
Sanket Patel <sanket@qpme.com>
2024-01-12 10:36:35 -0500

    added few more tests

--- postgres-flex/conftest.py
+++ postgres-flex/conftest.py
@@ -81,6 +81,23 @@ def dql_user_connection(connection_params):
         if new_conn:
             new_conn.close()
 
+@pytest.fixture(scope='module')
+def dml_user_connection(connection_params):
+    """
+    Fixture that returns postgres connection as a read_only user
+    """
+    try:
+        new_conn = None
+        new_conn = common.get_db_connection(host=connection_params['host'],
+                    port=connection_params['port'],
+                    database=common.get_db_name(),
+                    user=common.get_app_dml_user(),
+                    password=connection_params['password'])
+        yield new_conn
+    finally:
+        if new_conn:
+            new_conn.close()
+
 
 @pytest.fixture(scope='module')
 def create_database_and_connect(admin_connection, connection_params):
--- postgres-flex/test_postgres.py
+++ postgres-flex/test_postgres.py
@@ -6,32 +6,39 @@ import pytest
 import psycopg2
 from psycopg2.errors import InsufficientPrivilege
 
-class TestPostgres:
-    def test_connection(self, db_connection):
+class TestPostgresEngine:
+    """
+    A collection of tests to verify postgres engine functionality
+    """
+
+    def test_connect_as_dml_user(self, dml_user_connection):
         """
-        Tests database connection
+        Verifies if a dml_user can connect to database or not
         """
-        print ("testing connection")
-        with db_connection.cursor() as cur:
-            cur = db_connection.cursor()
-            print('PostgreSQL database version:')
+        assert dml_user_connection is not None
+        print (dml_user_connection)
+        assert common.get_app_dml_user() == dml_user_connection.get_dsn_parameters().get('user')
+
+        with dml_user_connection.cursor() as cur:
             cur.execute('SELECT version()')
             db_version = cur.fetchone()
-            print ("db version: %s" % db_version)
             assert "15.5" in db_version[0]
 
-    # def test_select(self, db_connection):
-    #     """
-    #     Tests select query
-    #     """
-    #     with db_connection.cursor() as cur:
-    #         print ('SELECT * from %s' % common.get_article_table_name());
-    #         cur.execute('SELECT * from %s' % common.get_article_table_name())
-    #         x = cur.fetchall()
-    #         print (x)
-    #         assert 2 == len(x)
-    #         assert 'hello_postgres' == x[0][1]
-    #         assert 'hello_redis' == x[1][1]
+    def test_connect_as_ddl_user(self, ddl_user_connection):
+        """
+        Verifies if a ddl_user can connect to database or not
+        """
+        assert ddl_user_connection is not None
+        print (ddl_user_connection)
+        assert common.get_app_ddl_user() == ddl_user_connection.get_dsn_parameters().get('user')
+
+    def test_connect_as_dql_user(self, dql_user_connection):
+        """
+        Verifies if a dql_user can connect to database or not
+        """
+        assert dql_user_connection is not None
+        print (dql_user_connection)
+        assert common.get_app_dql_user() == dql_user_connection.get_dsn_parameters().get('user')
 
     def test_ddl_user_can_create_tables(self, ddl_user_connection):
         """
@@ -40,30 +47,34 @@ class TestPostgres:
         with ddl_user_connection.cursor() as cur:
             x = cur.execute(self.__create_comments_table())
             print (x)
-        
+      
         with ddl_user_connection.cursor() as cur:
             x = cur.execute(self.__delete_comments_table())
             print (x)
         
+    ## TODO: should we add truncate permission to DDL role?
+    def test_ddl_user_can_truncate_tables(self, ddl_user_connection):
+        """
+        Tests access of app_ddl_user
+        """
+        # Make sure that data exists
+        with ddl_user_connection.cursor() as cur:
+            cur.execute("SELECT * from %s" % common.get_article_table_name())
+            x = cur.fetchall()
+            assert 2 == len(x)
+            assert 'hello_postgres' == x[0][1]
+            assert 'hello_redis' == x[1][1]         
 
-    # def test_ddl_user_can_truncate_tables(self, ddl_user_connection, dql_user_connection):
-    #     """
-    #     Tests access of app_ddl_user
-    #     """
-
-    #     # Make sure that data exists
-    #     with dql_user_connection.cursor() as cur:
-    #         cur.execute("SELECT * from %s" % common.get_article_table_name())
-    #         x = cur.fetchall()
-    #         assert 2 == len(x)
-    #         assert 'hello_postgres' == x[0][1]
-    #         assert 'hello_redis' == x[1][1]            
-
-    #     # Clear all the data
-    #     with ddl_user_connection.cursor() as cur:
-    #         x = cur.execute(self.__truncate_table_stmt())
-    #         print (x)     
-
+        # Clear all the data
+        with ddl_user_connection.cursor() as cur:
+            x = cur.execute(self.__truncate_table_stmt())
+            print (x)
+        
+        with ddl_user_connection.cursor() as cur:
+            cur.execute("SELECT * from %s" % common.get_article_table_name())
+            x = cur.fetchall()
+            assert 0 == len(x)
+            
     def test_ddl_user_can_delete_tables(self, ddl_user_connection):
         """
         Tests access of app_ddl_user
@@ -80,6 +91,13 @@ class TestPostgres:
             x = cur.execute(self.__alter_table_stmt())
             print (x)
 
+    def xtest_dml_user_can_insert(self, dml_user_connection):
+        """
+        """
+        with dml_user_connection.cursor() as cur:
+            x = cur.execute(self.__insert_stmt())
+            print(x)
+
     def test_dql_user_cannot_create_tables(self, dql_user_connection):
         """
         Tests that dql_user shouldn't be able to create the tables
@@ -89,16 +107,16 @@ class TestPostgres:
                 cur.execute(self.__create_comments_table())
         assert 'permission denied' in str(e)                
 
-    # def test_dql_user_can_select(self, dql_user_connection):
-    #     """
-    #     Tests that dql_user should be able to run select queries
-    #     """
-    #     with dql_user_connection.cursor() as cur:
-    #         print ('SELECT * from %s' % common.get_article_table_name())
-    #         cur.execute('SELECT * from %s' % common.get_article_table_name())
-    #         x = cur.fetchall()
-    #         print (x)
-    #         assert 2 == len(x)
+    def xtest_dql_user_can_select(self, dql_user_connection):
+        """
+        Tests that dql_user should be able to run select queries
+        """
+        with dql_user_connection.cursor() as cur:
+            print ('SELECT * from %s' % common.get_article_table_name())
+            cur.execute('SELECT * from %s' % common.get_article_table_name())
+            x = cur.fetchall()
+            print (x)
+            assert 2 == len(x)
 
     def test_dql_user_cannot_insert(self, dql_user_connection):
         """
